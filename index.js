d20c54d276b1aeb027577fdb5624978e568aeaa8
David Herbert <daviehert@gmail.com>
2022-10-24 18:24:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(express.static(__dirname + "/static"));
+
+app.get("/feed", async (req, res) => {
+  const todos = await prisma.todo.findMany();
+  res.json(todos);
+});
+
+app.get("/search", async (req, res) => {
+  const todos = await prisma.todo.findMany({
+    where: {
+      body: {
+        search: req.query.q,
+      },
+    },
+  });
+  res.json(todos);
+});
+
+app.listen(3000, () => console.log("Server started"));
